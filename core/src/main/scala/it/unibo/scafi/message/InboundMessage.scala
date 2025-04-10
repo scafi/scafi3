4cd0233f02b3fa4581a7b7a91d067baee1adaf93
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-10 16:35:46 +0200

    on messages and network

---
+++ core/src/main/scala/it/unibo/scafi/message/InboundMessage.scala
@@ -0,0 +1,44 @@
+package it.unibo.scafi.message
+
+import it.unibo.scafi.runtime.context.AggregateContext
+import it.unibo.scafi.utils.Stack
+import language.experimental.saferExceptions
+
+trait InboundMessage[DeviceId]:
+  self: Stack & AggregateContext[DeviceId] =>
+
+  private lazy val cachedPaths = new CachedPaths(importFromInboundMessages)
+
+  protected def alignedDevices: Iterable[DeviceId] =
+    if currentPath.isEmpty then cachedPaths.neighbors else cachedPaths.alignedDevicesAt(currentPath)
+
+  protected def alignedMessages[Value]: Map[DeviceId, Value] = cachedPaths.dataAt(currentPath)
+
+  private class CachedPaths(private val input: Import[DeviceId]):
+    private lazy val cachedPaths: Map[Path[?], Map[DeviceId, Any]] =
+      input.foldLeft(Map.empty):
+        case (accumulator, (deviceId, valueTree)) =>
+          valueTree.paths.foldLeft(accumulator): (accInner, path) =>
+            val valueAtPath =
+              try valueTree(path)
+              catch
+                case _: valueTree.NoPathFoundException =>
+                  throw RuntimeException(s"Path: $path not found, this should not happen. Please report this.")
+            accInner.updatedWith(path):
+              case Some(existing) => Some(existing + (deviceId -> valueAtPath))
+              case None => Some(Map(deviceId -> valueAtPath))
+
+    lazy val neighbors: Iterable[DeviceId] = input.keySet + localId
+
+    def alignedDevicesAt(tokens: IndexedSeq[InvocationCoordinate]): Iterable[DeviceId] =
+      cachedPaths
+        .filter: (p, _) =>
+          p.startsWith(tokens)
+        .values
+        .flatMap(_.keySet)
+        .toSet + localId
+
+    def dataAt[Value](tokens: IndexedSeq[InvocationCoordinate]): Map[DeviceId, Value] =
+      cachedPaths.get(Path(tokens *)).map(_.view.mapValues(_.asInstanceOf[Value]).toMap).getOrElse(Map.empty)
+  end CachedPaths
+end InboundMessage
