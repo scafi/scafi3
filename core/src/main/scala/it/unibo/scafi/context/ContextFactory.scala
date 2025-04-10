c8bae09077fd9704b212e74f2f6d539e4b7e9da1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-10 22:31:12 +0200

    style

--- core/src/main/scala/it/unibo/scafi/context/ContextFactory.scala
+++ core/src/main/scala/it/unibo/scafi/context/ContextFactory.scala
@@ -1,6 +1,10 @@
 package it.unibo.scafi.context
 
 import it.unibo.scafi.runtime.network.NetworkManager
- 
-trait ContextFactory[ID, Net <: NetworkManager { type DeviceId = ID }, Context <: AggregateContext { type DeviceId = ID }]:
+
+trait ContextFactory[
+    ID,
+    Net <: NetworkManager { type DeviceId = ID },
+    Context <: AggregateContext { type DeviceId = ID },
+]:
   def createContext(deviceId: ID, network: Net): Context
