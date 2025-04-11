e41ecfd5f5a19368418825aa16d1504711dc919f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-11 22:03:45 +0200

    test: add basic test for value tree

--- scafi-core/src/main/scala/it/unibo/scafi/message/Path.scala
+++ scafi-core/src/main/scala/it/unibo/scafi/message/Path.scala
@@ -3,4 +3,6 @@ package it.unibo.scafi.message
 trait Path[+Token] extends IndexedSeq[Token]
 
 object Path:
-  def apply[Token](tokens: Token*): Path[Token] = ???
+  def apply[Token](tokens: Token*): Path[Token] = new Path[Token]:
+    override def apply(i: Int): Token = tokens(i)
+    override def length: Int = tokens.length
