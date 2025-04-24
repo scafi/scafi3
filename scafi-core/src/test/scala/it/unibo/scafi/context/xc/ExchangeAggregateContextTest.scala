d80d2a76fce06cf958107836e7fc0af14e59c6a2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-24 23:04:23 +0200

    test: basic test for XC

--- scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
+++ scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
@@ -1,12 +1,13 @@
 package it.unibo.scafi.context.xc
 
-import it.unibo.scafi.message.Import
-import org.scalatest.flatspec.AnyFlatSpecLike
-import org.scalatest.matchers.should
+import it.unibo.scafi.message.{ Import, ValueTree }
 import it.unibo.scafi.libraries.All.{ *, given }
-import cats.syntax.all.*
+import it.unibo.scafi.message.ValueTree.NoPathFoundException
 import it.unibo.scafi.utils.AggregateProgramProbe
 import it.unibo.scafi.utils.network.NoNeighborsNetworkManager
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should
 
 class ExchangeAggregateContextTest extends AnyFlatSpecLike, should.Matchers, AggregateProgramProbe:
   type Lang = ExchangeAggregateContext[Int]
@@ -17,4 +18,7 @@ class ExchangeAggregateContextTest extends AnyFlatSpecLike, should.Matchers, Agg
   "Exchange construct" should "return a different value than the one sent" in:
     def programRetSend(using Lang) = exchange(localId) { x => returning(x.map(_ + 1)) send x }.default
     val (result, exportValue) = roundForAggregateProgram(0, factory)(programRetSend)
-    result should be(1)
+    result shouldBe 1
+    val singlePath = exportValue(0).paths.head // There is only one path
+    try exportValue(0).apply(singlePath) shouldBe 0
+    catch case _: NoPathFoundException => fail("Path not found")
