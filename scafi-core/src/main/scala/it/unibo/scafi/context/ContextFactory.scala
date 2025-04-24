bd307470dcdc93d0464ac88fe926400120dd3b1d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-24 22:19:04 +0200

    refactor: add functional interface annotation

--- scafi-core/src/main/scala/it/unibo/scafi/context/ContextFactory.scala
+++ scafi-core/src/main/scala/it/unibo/scafi/context/ContextFactory.scala
@@ -2,6 +2,7 @@ package it.unibo.scafi.context
 
 import it.unibo.scafi.runtime.network.NetworkManager
 
+@FunctionalInterface
 trait ContextFactory[
     ID,
     Network <: NetworkManager { type DeviceId = ID },
