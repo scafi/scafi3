b6a50feebae01ef45d1ce98d0fa1a3abc1f4ae62
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-04-27 20:05:55 +0200

    minor

--- scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
+++ scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
@@ -1,19 +1,31 @@
 package it.unibo.scafi.context.xc
 
 import it.unibo.scafi.context.common.BranchingContextTest
-import it.unibo.scafi.message.Import
+import it.unibo.scafi.message.{ Import, ValueTree }
 import it.unibo.scafi.libraries.All.*
 import it.unibo.scafi.message.ValueTree.NoPathFoundException
 import it.unibo.scafi.utils.AggregateProgramProbe
 import it.unibo.scafi.utils.network.NoNeighborsNetworkManager
+
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should
 
-class ExchangeAggregateContextTest extends AnyFlatSpecLike, should.Matchers, AggregateProgramProbe, BranchingContextTest:
+class ExchangeAggregateContextTest
+    extends AnyFlatSpecLike,
+      should.Matchers,
+      AggregateProgramProbe,
+      BranchingContextTest:
   type Lang = ExchangeAggregateContext[Int]
   private class ExchangeContextTest(id: Int, inbound: Import[Int]) extends ExchangeAggregateContext[Int](id, inbound)
 
-  "ExchangeContext" should behave like branchingSemantic(using ExchangeContextTest(0, Import.empty))
+  "ExchangeContext" should behave like branchingSemantic(using
+    ExchangeContextTest(
+      0,
+      Import(
+        Map(1 -> ValueTree.empty),
+      ),
+    ),
+  )
 
   "Exchange construct" should "return a different value than the one sent" in:
     def programRetSend(using Lang) = exchange(localId) { x => returning(x.map(_ + 1)) send x }.default
@@ -23,3 +35,4 @@ class ExchangeAggregateContextTest extends AnyFlatSpecLike, should.Matchers, Agg
     val singlePath = exportValue(0).paths.head // There is only one path
     try exportValue(0).apply[Int](singlePath) shouldBe 0
     catch case _: NoPathFoundException => fail("The path should exist, but it was not found.")
+end ExchangeAggregateContextTest
