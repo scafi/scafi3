94262daffac146d4b8d5c38b95a7d59658caf166
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-05-04 15:14:49 +0200

    test: add FC test

--- scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
+++ scafi-core/src/test/scala/it/unibo/scafi/context/xc/ExchangeAggregateContextTest.scala
@@ -1,12 +1,12 @@
 package it.unibo.scafi.context.xc
 
-import it.unibo.scafi.context.common.BranchingContextTest
-import it.unibo.scafi.message.{ Import, ValueTree }
-import it.unibo.scafi.libraries.All.*
+import it.unibo.scafi.context.xc.ExchangeAggregateContext.exchangeContextFactory
+import it.unibo.scafi.language.common.syntax.BranchingSyntaxTest
+import it.unibo.scafi.language.fc.syntax.FieldCalculusSyntaxTest
 import it.unibo.scafi.message.ValueTree.NoPathFoundException
+import it.unibo.scafi.libraries.All.*
 import it.unibo.scafi.utils.AggregateProgramProbe
 import it.unibo.scafi.utils.network.NoNeighborsNetworkManager
-
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should
 
@@ -14,16 +14,17 @@ class ExchangeAggregateContextTest
     extends AnyFlatSpecLike,
       should.Matchers,
       AggregateProgramProbe,
-      BranchingContextTest:
+      BranchingSyntaxTest,
+      FieldCalculusSyntaxTest:
   type Lang = ExchangeAggregateContext[Int]
-  private class ExchangeContextTest(id: Int, inbound: Import[Int]) extends ExchangeAggregateContext[Int](id, inbound)
 
-  "ExchangeContext" should behave like branchingSemantic(using ExchangeContextTest(0, Import.empty))
+  "ExchangeContext" should behave like branchSpecification(exchangeContextFactory)
+  it should behave like fieldCalculusSpecification(exchangeContextFactory)
 
   "Exchange construct" should "return a different value than the one sent" in:
     def programRetSend(using Lang) = exchange(localId) { x => returning(x.map(_ + 1)) send x }.default
     val (result, exportValue) =
-      roundForAggregateProgram(0, (id, _) => ExchangeContextTest(id, Import.empty))(programRetSend)
+      roundForAggregateProgram(0, NoNeighborsNetworkManager[Int](), exchangeContextFactory)(programRetSend)
     result shouldBe 1
     val singlePath = exportValue(0).paths.head // There is only one path
     try exportValue(0).apply[Int](singlePath) shouldBe 0
