f535552c837702be02cbd0af78d7803618bd2173
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-05-04 15:07:03 +0200

    refactor: rename to ID

--- scafi-core/src/test/scala/it/unibo/scafi/utils/network/NoNeighborsNetworkManager.scala
+++ scafi-core/src/test/scala/it/unibo/scafi/utils/network/NoNeighborsNetworkManager.scala
@@ -6,12 +6,12 @@ import it.unibo.scafi.runtime.network.NetworkManager
 /**
  * This class is meant to be used when no neighbors are present in the network. It is used to test the behavior of the
  * program itself.
- * @tparam Id
+ * @tparam ID
  *   the type of the deviceId of neighbor devices.
  */
-class NoNeighborsNetworkManager[Id] extends NetworkManager:
-  override type DeviceId = Id
+class NoNeighborsNetworkManager[ID] extends NetworkManager:
+  override type DeviceId = ID
 
-  override def receive: Import[Id] = Import.empty
+  override def receive: Import[ID] = Import.empty
 
-  override def send(message: Export[Id]): Unit = ()
+  override def send(message: Export[ID]): Unit = ()
