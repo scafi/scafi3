97514dae45ce155d06ff5fda099d0174d94cc3f2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-05-06 01:12:36 +0200

    fix: implement equals and hash code

--- scafi-core/src/main/scala/it/unibo/scafi/message/Path.scala
+++ scafi-core/src/main/scala/it/unibo/scafi/message/Path.scala
@@ -7,5 +7,13 @@ object Path:
     override def apply(i: Int): Token = tokens(i)
     override def length: Int = tokens.length
 
+    override def equals(o: Any): Boolean = o match
+      case that: Path => this.toString == that.toString
+      case _ => false
+
+    override def hashCode: Int = tokens.hashCode()
+
+    override def toString: String = tokens.mkString("Path(", ", ", ")")
+
   given CanEqual[Path, Path] = CanEqual.derived
   given CanEqual[Iterable[Path], Iterable[Path]] = CanEqual.derived
