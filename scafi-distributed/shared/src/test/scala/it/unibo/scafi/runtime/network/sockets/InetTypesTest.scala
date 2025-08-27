e48b1a77cd332fe49500e418876edca7be6fa392
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-08-27 22:28:59 +0200

    style: import reordering

--- scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/InetTypesTest.scala
+++ scafi-distributed/shared/src/test/scala/it/unibo/scafi/runtime/network/sockets/InetTypesTest.scala
@@ -1,12 +1,13 @@
 package it.unibo.scafi.runtime.network.sockets
 
 import it.unibo.scafi
-import scafi.runtime.network.sockets.InetTypes.{ Hostname, IPv4, IPv6 }
 
 import io.github.iltotore.iron.*
 import org.scalatest.Inspectors
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+
+import scafi.runtime.network.sockets.InetTypes.{ Hostname, IPv4, IPv6 }
 
 class InetTypesTest extends AnyFlatSpec with should.Matchers with Inspectors:
 
