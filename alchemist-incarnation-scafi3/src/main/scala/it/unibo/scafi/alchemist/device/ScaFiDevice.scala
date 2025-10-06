6a6ccff6125148a809e853a2100d9986df9a85de
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-06 12:27:51 +0200

    minor fixes in incarnation and device implementation

--- alchemist-incarnation-scafi3/src/main/scala/it/unibo/scafi/alchemist/device/ScaFiDevice.scala
+++ alchemist-incarnation-scafi3/src/main/scala/it/unibo/scafi/alchemist/device/ScaFiDevice.scala
@@ -1,41 +1,50 @@
 package it.unibo.scafi.alchemist.device
 
-import it.unibo.alchemist.model.{ Position as AlchemistPosition, * }
-import it.unibo.scafi.engine.network.{ Export, Import, Network }
-import it.unibo.scafi.alchemist.TimeUtils.given Ordering[Time]
+import it.unibo.alchemist.model.{Environment, Node, NodeProperty, Time, Position as AlchemistPosition}
+import it.unibo.scafi.message.{Export, Import}
+import it.unibo.scafi.runtime.network.NetworkManager
+import org.apache.commons.math3.random.RandomGenerator
 
 import math.Ordering.Implicits.infixOrderingOps
 
 class ScaFiDevice[T, Position <: AlchemistPosition[Position], ExportValue](
-    val node: Node[T],
+    val random: RandomGenerator,
     val env: Environment[T, Position],
-    val retention: Time,
-) extends Network[Int, ExportValue]
-    with NodeProperty[T]:
+    val node: Node[T],
+    val retention: Time | Null,
+) extends NetworkManager,
+      NodeProperty[T]:
+
+  override type DeviceId = Int
+
   private var inbox: Map[Int, TimedMessage[ExportValue]] = Map.empty
 
-  private def time: Time = env.getSimulation.nn.getTime.nn // TODO: maybe it should be a public var
+  private def time: Time = ??? // env.getSimulation.nn.getTime.nn // TODO: maybe it should be a public var
 
   override def localId: Int = node.getId
 
-  override def send(e: Export[Int, ExportValue]): Unit =
-    inbox += localId -> TimedMessage(time, e(localId))
-    env
-      .getNeighborhood(node)
-      .nn
-      .forEach: n =>
-        val node: Node[T] = n.nn
-        node.asProperty(classOf[ScaFiDevice[T, Position, ExportValue]]).inbox += localId -> TimedMessage(
-          time, // TODO: current impl uses time of the sender
-          e(localId),
-        )
+//  override def send(e: Export[Int, ExportValue]): Unit = ???
+//    inbox += localId -> TimedMessage(time, e(localId))
+//    env
+//      .getNeighborhood(node)
+//      .nn
+//      .forEach: n =>
+//        val node: Node[T] = n.nn
+//        node.asProperty(classOf[ScaFiDevice[T, Position, ExportValue]]).inbox += localId -> TimedMessage(
+//          time, // TODO: current impl uses time of the sender
+//          e(localId),
+//        )
 
-  override def receive(): Import[Int, ExportValue] =
-    inbox = inbox.filterNot(_._2.time.plus(retention) < time)
-    inbox.map((id, timedMessage) => id -> timedMessage.message)
+//  override def receive(): Import[Int, ExportValue] =
+//    inbox = inbox.filterNot(_._2.time.plus(retention) < time)
+//    inbox.map((id, timedMessage) => id -> timedMessage.message)
+
+  override def send(message: Export[Int]): Unit = ???
+
+  override def receive: Import[Int] = ???
 
   override def getNode: Node[T] = node
 
   override def cloneOnNewNode(node: Node[T]): NodeProperty[T] =
-    ScaFiDevice(node, env, retention)
+    ScaFiDevice(random, env, node, retention)
 end ScaFiDevice
