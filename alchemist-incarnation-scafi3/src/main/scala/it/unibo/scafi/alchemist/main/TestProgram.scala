f6065651d29ba9c17f5294cdb27bd498de437b77
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-10 16:13:25 +0200

    Add example

--- alchemist-incarnation-scafi3/src/main/scala/it/unibo/scafi/alchemist/main/TestProgram.scala
+++
@@ -1,9 +0,0 @@
-package it.unibo.scafi.alchemist.main
-
-
-object TestProgram//:
-//  type MyLanguage = AggregateFoundation { type DeviceId = Int } & FieldCalculusSyntax & DistanceSensor[Double]
-//  def myProgram(using MyLanguage): Int = evolve(0)(_ + 1)
-//
-//  def myProgram2(using MyLanguage): Double =
-//    sensorDistanceTo(localId == 0)
