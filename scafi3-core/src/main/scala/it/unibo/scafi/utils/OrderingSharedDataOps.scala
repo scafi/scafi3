152b9e1cad9b515cbdb98bca147eee0b260fc600
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-10-24 09:23:20 +0200

    fix: provide extension methods only for neighbor values data

--- scafi3-core/src/main/scala/it/unibo/scafi/utils/OrderingSharedDataOps.scala
+++ scafi3-core/src/main/scala/it/unibo/scafi/utils/OrderingSharedDataOps.scala
@@ -1,9 +1,9 @@
 package it.unibo.scafi.utils
 
-import it.unibo.scafi.language.AggregateFoundation
+import it.unibo.scafi.language.xc.FieldBasedSharedData
 
 object OrderingSharedDataOps:
-  extension [V: Ordering as ordering](using lang: AggregateFoundation)(data: lang.SharedData[V])
+  extension [V: Ordering as ordering](using lang: FieldBasedSharedData)(data: lang.SharedData[V])
     infix def compare(that: lang.SharedData[V]): lang.SharedData[Int] = data.alignedMap(that)(ordering.compare)
     infix def min(that: lang.SharedData[V]): lang.SharedData[V] = data.alignedMap(that)(ordering.min)
     infix def max(that: lang.SharedData[V]): lang.SharedData[V] = data.alignedMap(that)(ordering.max)
