d33c6013f07a193f75d6a21389c83909008c420f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-06-15 15:53:12 +0200

    refactor: removed unused given and removed deprecated App trait

--- example/src/main/scala/it/unibo/scafi/GradientInline.scala
+++ example/src/main/scala/it/unibo/scafi/GradientInline.scala
@@ -2,8 +2,10 @@ package it.unibo.scafi
 
 import it.unibo.alchemist.boundary.LoadAlchemist
 
-object GradientInline extends App:
-  val simulationFile = getClass.getResource("/it/unibo/scafi/inline-gradient.yml").getPath
-  val loader = LoadAlchemist.from(simulationFile)
-  val simulation = loader.getDefault[Any, Nothing]()
-  simulation.run()
+object GradientInline:
+  @main
+  def runInline(): Unit =
+    val simulationFile = getClass.getResource("/it/unibo/scafi/inline-gradient.yml").getPath
+    val loader = LoadAlchemist.from(simulationFile)
+    val simulation = loader.getDefault[Any, Nothing]()
+    simulation.run()
